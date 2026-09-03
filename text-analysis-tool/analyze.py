770384897ccbb0877b1e95477a62d8d29128f2d5
chika-12 <markworship001@gmail.com>
2026-09-03 15:41:01 +0100

    completing file reading capability

--- text-analysis-tool/analyze.py
+++ text-analysis-tool/analyze.py
@@ -2,11 +2,22 @@ from random_username.generate import generate_username
 def welcome():
     print("Welcome to the text analysis tool")
 
+
 def callUserName():
-    username = input("Enter your username: ")
-    
-    if len(username) > 4 and  username.isidentifier():
-        return username
+    maxAttempt = 3
+    attempt = 0
+    while attempt < maxAttempt:
+        
+        inputPrompt = ""
+        if attempt == 0:
+            inputPrompt = "\n To begin enter username\n"
+        else:
+            inputPrompt = "\n Please try again\n"
+        username = input(inputPrompt)
+        
+        if len(username) > 4 and  username.isidentifier():
+            return username
+        attempt += 1
     
     print("The username you choosed is invalid a new username has been Assigned to you")
     username = generate_username()[0]
@@ -17,7 +28,14 @@ def callUserName():
 def greetUser(name):
     print("Hello ", name)
     
-    
+def text_reader():
+    text = open("./files/financial_analysis.txt")
+    out_put_text = text.read()
+    text.close()
+    return out_put_text.replace('\n', '').replace('\r', '')
+
 welcome()
 username = callUserName()
 greetUser(username)
+text_for_analysis = text_reader()
+print(text_for_analysis)
